d58c7de7082bbc3257ed7437a65eb2da68698902
Pablo Martínez <pablomartinez1017@gmail.com>
2020-03-20 22:21:22 +0100

    FUNCIONA

---
+++ script.sh
@@ -0,0 +1,3 @@
+./jmeter.sh -n -t Funciona.jmx -l scriptresults75.jtl -j registro75.log -JHILOS=75 -JSUBIDA=30 -JDURACION=270 -JFILE=scriptresultsperfmon75.jtl
+./jmeter.sh -n -t Funciona.jmx -l scriptresults150.jtl -j registro150.log -JHILOS=150 -JSUBIDA=60 -JDURACION=300 -JFILE=scriptresultsperfmon150.jtl
+./jmeter.sh -n -t Funciona.jmx -l scriptresults300.jtl -j registro300.log -JHILOS=300 -JSUBIDA=120 -JDURACION=360 -JFILE=scriptresultsperfmon300.jtl

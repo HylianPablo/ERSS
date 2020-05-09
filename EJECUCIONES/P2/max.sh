34aa0301d3652ae8198738aa348b71dc7a3afc9b
Pablo Martínez <pablomartinez1017@gmail.com>
2020-05-09 18:33:12 +0200

    nuevas 160

--- EJECUCIONES/P2/max.sh
+++
@@ -1,2 +0,0 @@
-../../jmeter.sh -n -t ../../Dinamica.jmx -l scriptresultsDin500.jtl -j registroDin500.log -DHILOS1=167 -DDELAY1=0 -DSUBIDA1=45 -DDURACION1=495 -DHILOS2=166 -DDELAY2=30 -DSUBIDA2=45 -DDURACION2=330 -DHILOS3=166 -DDELAY3=90 -DSUBIDA3=45 -DDURACION3=165 -DFILE=scriptresultsperfmonDin500.jtl
-
--- EJECUCIONES/P2/prueba.sh
+++
@@ -1,3 +0,0 @@
-ulimit -S -n unlimited
-ulimit -n
-../../jmeter.sh -n -t ../../Dinamica.jmx -l scriptresultsDin15.jtl -j registroDin15.log -DHILOS=15 -DSUBIDA=5 -DDURACION=125 -DFILE=scriptresultsperfmonDin15.jtl

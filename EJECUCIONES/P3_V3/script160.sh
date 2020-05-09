08cc96a3b98db5745693f9daf625a63263d178d7
Pablo Martínez <pablomartinez1017@gmail.com>
2020-05-09 19:59:42 +0200

    Reorganizacion

--- EJECUCIONES/P3_V3/script160.sh
+++
@@ -1,3 +0,0 @@
-ulimit -S -n unlimited
-ulimit -n
-../../jmeter.sh -n -t ../RecordP3_V2.jmx -l scriptresults160.jtl -j registro160.log -DHILOS=160 -DSUBIDA=25 -DDURACION=625 -DFILE=scriptresultsPerfmon160.jtl
